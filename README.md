63d1d391ca90434c0c7c81d9a79917afdbc4b83e
Madeleine Ostoja <madi@madeleineostoja.com>
2022-07-04 13:23:16 +1200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Imgix SrSet Generator
+
+[![NPM](https://img.shields.io/npm/v/imgix-srcset)](https://www.npmjs.com/package/imgix-srcset) ![npm bundle size](https://img.shields.io/bundlephobia/minzip/imgix-srcset?label=size) [![License](https://img.shields.io/npm/l/imgix-srcset)] [![License](https://img.shields.io/npm/l/imgix-srcset)](https://github.com/madeleineostoja/imgix-srcset/blob/master/LICENSE.md)
+
+A dead simple utility to generate responsive srcsets from an Imgix compatible source URL
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { stringify } from 'query-string';
+
+type ImgixProps = { [key: string]: string };
+
+export function trimSrc(src: string) {
+  return src.split(/[?#]/)[0];
+}
+
+export function srcset(src: string, imgixProps: ImgixProps = {}) {
+  const resolutions: number[] = [],
+    sets = [];
+
+  let prev = 100;
+
+  while (prev <= 8192) {
+    resolutions.push(2 * Math.round(prev / 2));
+    prev *= 1 + 0.08 * 2;
+  }
+
+  for (var i = 0; i < resolutions.length; i++) {
+    const params = {
+      w: resolutions[i],
+      ...imgixProps
+    };
+
+    sets.push(`${trimSrc(src)}?${stringify(params)} ${resolutions[i]}w`);
+  }
+
+  return sets.join(', ');
+}
